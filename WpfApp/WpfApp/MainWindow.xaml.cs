d8c0c001475ca80f7f864efe7d0b57f1494131d5
FW960 <artuninp@gmail.com>
2022-09-21 03:01:09 +0800

    Refactored

--- WpfApp/WpfApp/MainWindow.xaml.cs
+++ WpfApp/WpfApp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,62 @@ namespace WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Thread? _thread;
+
+        private MyFibNum? _myFibNumWpf;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_thread is not null)
+            {
+                _thread.Interrupt();
+            }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            _myFibNumWpf = new MyFibNum();
+
+            _thread = new Thread(Process) {IsBackground = true};
+
+            _thread.Start();
+        }
+
+        private void ContinueButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_myFibNumWpf is not null && !_thread.IsAlive)
+            {
+                _thread = new Thread(Process) {IsBackground = true};
+
+                _thread.Start();
+            }
+        }
+
+        private void Process()
+        {
+            while (true)
+            {
+                try
+                {
+                    ulong value = _myFibNumWpf.CalculateNext();
+
+                    TextBlockFib.Dispatcher.BeginInvoke(() =>
+                    {
+                        TextBlockFib.Text = $"Number: {_myFibNumWpf.FibNums.Count - 1}\r\nValue: {value}";
+                    });
+
+                    Thread.Sleep(1000);
+                }
+                catch (ThreadInterruptedException)
+                {
+                    return;
+                }
+            }
         }
     }
 }
---
+++ WpfApp/WpfApp/MyFibNum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace WpfApp;
+
+public class MyFibNum
+{
+    public List<ulong> FibNums;
+
+    public MyFibNum()
+    {
+        FibNums = new List<ulong>();
+
+        FibNums.Add(0);
+
+        FibNums.Add(1);
+    }
+
+    public ulong CalculateNext()
+    {
+        ulong lastValue = FibNums[FibNums.Count - 1] + FibNums[FibNums.Count - 2];
+        FibNums.Add(lastValue);
+
+        return lastValue;
+    }
+}
